c13b8ede10f82faaa54fd9d625bf06fc575c9fd9
wzf2000 <nbjcwzf@163.com>
2024-05-13 23:09:31 +0800

    Add marker for AMap.

--- src/app/components/MapContainer.js
+++ src/app/components/MapContainer.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./MapContainer.css";
 import AMapLoader from "@amap/amap-jsapi-loader";
+import { title } from "process";
 
 export default function MapContainer(props) {
   let map = null;
@@ -10,12 +11,41 @@ export default function MapContainer(props) {
   let lat = props.lat;
   let [globalAMap, setGlobalAMap] = useState(null);
 
+  let stylesArray = [
+    {
+      icon: { //图标样式
+        img: "//vdata.amap.com/icons/b18/1/2.png",
+        size: [16, 16], //图标的原始大小
+        anchor: "bottom-center", //锚点位置
+        fitZoom: 14, //最合适的级别 在此级别显示为图标原始大小
+        scaleFactor: 2, //地图放大一级的缩放比例系数 
+        maxScale: 2, //图片的最大放大比例，随着地图放大图标会跟着放大，最大为2
+        minScale: 1, //图片的最小缩小比例，随着地图缩小图标会跟着缩小，最小为1
+      }
+    }
+  ];
+
+  var zoomStyleMapping = {
+    14: 0,
+    15: 0,
+    16: 0,
+    17: 0,
+    18: 0,
+    19: 0,
+    20: 0,
+  };
+
   let drawMap = (AMap) => {
-    if (city === '城市未知' || city === undefined || address === '地点待确定' || address === undefined)
+    if (
+      city === "城市未知" ||
+      city === undefined ||
+      address === "地点待确定" ||
+      address === undefined
+    )
       return;
-    AMap.plugin('AMap.Geocoder', function () {
+    AMap.plugin("AMap.Geocoder", function () {
       let geocoder = new AMap.Geocoder({
-        'city': city,
+        city: city,
       });
       if (lng !== undefined && lat !== undefined) {
         map = new AMap.Map("container", {
@@ -24,21 +54,41 @@ export default function MapContainer(props) {
           zoom: 15, // 初始化地图级别
           center: [lng, lat], // 初始化地图中心点位置
         });
+        AMap.plugin(["AMap.ElasticMarker"], function () {
+          var elasticMarker = new AMap.ElasticMarker({
+            title: address, //点标记的标题
+            position: [lng, lat], //点标记位置
+            styles: stylesArray, //指定样式列表
+            zoomStyleMapping: zoomStyleMapping, //指定 zoom 与样式的映射
+          });
+          map.add(elasticMarker); //添加到地图上
+          map.setFitView(); //缩放地图到合适的视野级别
+        });
       } else {
         geocoder.getLocation(address, function (status, result) {
-          if (status === 'complete' && result.info === 'OK') {
+          if (status === "complete" && result.info === "OK") {
             let location = result.geocodes[0].location;
             map = new AMap.Map("container", {
               // 设置地图容器id
               viewMode: "3D", // 是否为3D地图模式
-              zoom: 15, // 初始化地图级别
+              zoom: 10, // 初始化地图级别
               center: location, // 初始化地图中心点位置
+            });
+            AMap.plugin(["AMap.ElasticMarker"], function () {
+              var elasticMarker = new AMap.ElasticMarker({
+                title: address, //点标记的标题
+                position: location, //点标记位置
+                styles: stylesArray, //指定样式列表
+                zoomStyleMapping: zoomStyleMapping, //指定 zoom 与样式的映射
+              });
+              map.add(elasticMarker); //添加到地图上
+              map.setFitView(); //缩放地图到合适的视野级别
             });
           }
         });
       }
     });
-  }
+  };
 
   useEffect(() => {
     window._AMapSecurityConfig = {
@@ -48,7 +98,7 @@ export default function MapContainer(props) {
       AMapLoader.load({
         key: "c9020fcf56e3d78809895825c68f439e", // 申请好的Web端开发者Key，首次调用 load 时必填
         version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-        plugins: ['AMap.Geocoder'], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+        plugins: ["AMap.Geocoder", "AMap.ElasticMarker"], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
       })
         .then((AMap) => {
           setGlobalAMap(AMap);
@@ -57,10 +107,10 @@ export default function MapContainer(props) {
         .catch((e) => {
           console.log(e);
         });
-      } else {
-        console.log('AMap is already loaded');
-        drawMap(globalAMap);
-      }
+    } else {
+      console.log("AMap is already loaded");
+      drawMap(globalAMap);
+    }
 
     return () => {
       map?.destroy();
